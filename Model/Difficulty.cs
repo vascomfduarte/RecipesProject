269c612ed991276de3c7105ea3c08946c6b3ffdf
Vasco Duarte <vasco.duarte@students.assembly.pt>
2024-02-26 15:10:47 +0000

    Added SearchResults page in order to present the search results and leave the Search Page for a more in depth search.

--- Model/Difficulty.cs
+++ Model/Difficulty.cs
@@ -3,6 +3,10 @@ namespace Model
 {
     public class Difficulty
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
 
+        // Navigation properties
+        public List<Recipe> Recipes { get; set; }
     }
 }
