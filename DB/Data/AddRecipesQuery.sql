4cb65289388b9bef99c8f657229088779b61d4cc
Vasco Duarte <130570200+vascomfduarte@users.noreply.github.com>
2024-04-08 17:01:24 +0100

    Update AddRecipesQuery.sql

--- DB/Data/AddRecipesQuery.sql
+++ DB/Data/AddRecipesQuery.sql
@@ -11,55 +11,53 @@ INSERT INTO [dbo].[recipe]
            ,[user_id]
            ,[difficulty_id])
      VALUES
-           ('Spaghetti Carbonara', 'Classic Italian pasta dish with creamy egg and pancetta sauce.', 'https://n9.cl/vykrug', 20, 1, GETDATE(), 1, 2),
+           ('Spaghetti Carbonara', 'Classic Italian pasta dish with creamy egg and pancetta sauce.', 'https://t.ly/jjM54', 20, 1, GETDATE(), 104, 2),
 		   ('Chicken Alfredo Pasta',
         'Creamy pasta dish with grilled chicken, garlic, and Parmesan cheese.',
-        'https://n9.cl/lu2a9m',
+        'https://t.ly/dzljX',
         30,
         1,
         GETDATE(),
-        1, -- User ID is set to 1
+        104, -- User ID is set to 1
         3),
 		('Vegetable Stir-Fry',
         'Healthy and colorful stir-fried vegetables with tofu, served with rice.',
-        'https://n9.cl/ngv2b',
+        'https://t.ly/jJV6B',
         25,
         1,
         GETDATE(),
-        1, -- User ID is set to 1
+        104, -- User ID is set to 1
         2),
 		('Classic Chocolate Cake',
         'Decadent chocolate cake with rich chocolate frosting, perfect for celebrations.',
-        'https://n9.cl/ja5wz',
+        'https://t.ly/z_vCx',
         45,
         1,
         GETDATE(),
-        1, -- User ID is set to 1
+        104, -- User ID is set to 1
         3),
 		('Caprese Salad',
         'Simple and fresh salad made with ripe tomatoes, mozzarella cheese, basil, and balsamic vinegar.',
-        'https://n9.cl/4jqa4',
+        'https://rb.gy/jw4w1v',
         10,
         1,
         GETDATE(),
-        1, -- User ID is set to 1
+        104, -- User ID is set to 1
         1),
 		('Grilled Salmon with Lemon-Dill Sauce',
         'Delicious and healthy grilled salmon topped with a tangy lemon-dill sauce.',
-        'https://n9.cl/bumyo',
+        'https://rb.gy/o4e8wo',
         15,
         1,
         GETDATE(),
-        1, -- User ID is set to 1
+        104, -- User ID is set to 1
         2),
 		('Homemade Pizza Margherita',
         'Classic Italian pizza topped with fresh tomatoes, mozzarella cheese, and basil leaves.',
-        'https://n9.cl/dux41',
+        'https://rb.gy/aja67u',
         30,
         1,
         GETDATE(),
-        1, -- User ID is set to 1
+        104, -- User ID is set to 1
         2);
 GO
-
-
